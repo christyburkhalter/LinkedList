b9c4a4cc9792bc19d6ca80f3d11f595d0d16f294
Christy Burkhalter <53546957+christyburkhalter@users.noreply.github.com>
2019-10-13 16:32:30 -0500

    Add files via upload

---
+++ LinkedList/LinkedList1.rb
@@ -0,0 +1,45 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+
+class Stack
+  attr_reader :data
+
+  def initialize
+    @data = nil
+  end
+
+
+  def push(value)
+    @data = LinkedListNode.new(value, next_node)
+  end
+
+  def pop
+  end
+
+end
+
+
+
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} -->"
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+print_values(node3)
